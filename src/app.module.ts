6ff50fb7b7be46b28fe293b0a0d01c0a1bff1e95
Azdevpp <Azdevpp@gmail.com>
2022-12-29 21:57:03 +0200

    Adding the category to mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { DataSource } from 'typeorm';
 import { Post } from './post/entities/post.entity';
 import { CategoryModule } from './category/category.module';
 import { AuthModule } from './auth/auth.module';
+import { Category } from './category/entities/category.entity';
 
 @Module({
   imports: [
@@ -19,7 +20,7 @@ import { AuthModule } from './auth/auth.module';
       password: 'password',
       // database: 'yourdb',
       database: 'blog-tutorial',
-      entities: [Post],
+      entities: [Post, Category],
       synchronize: true,
     }),
     CategoryModule,
