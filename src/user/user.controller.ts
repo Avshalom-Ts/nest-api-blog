503a39d9fe550324c680db9fece2c01c92f59199
Azdevpp <Azdevpp@gmail.com>
2022-12-31 19:15:54 +0200

    Added login Method in the user.service and post(login) in the user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserLoginDto } from './dto/user-login.dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class UserController {
   constructor(private readonly authService: UserService) {}
+
+  @Post('login')
+  async userLogin(@Body() userLoginDto: UserLoginDto, @Res() res: Response) {
+    console.log(userLoginDto);
+    const { token, user } = await this.authService.login(userLoginDto);
+
+    res.cookie('IsAuthenticated', true, { maxAge: 2 * 60 * 60 * 1000 }); //Max age 2 hours
+    res.cookie('Authentication', token, {
+      httpOnly: true,
+      maxAge: 2 * 60 * 60 * 1000,
+    });
+
+    return res.send({ success: true, user });
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserLoginDto } from './dto/user-login.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly repo: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(loginDto: UserLoginDto) {
+    console.log(loginDto);
+    const user = await this.repo
+      .createQueryBuilder('user')
+      .addSelect('user.password')
+      .where('user.email = :email', { email: loginDto.email })
+      .getOne();
+    console.log(user);
+    if (!user) {
+      throw new UnauthorizedException('Bad credentials - user not found');
+    } else {
+      // verify that the supplied password is mathing with the hash password in dabaBase
+      if (await this.verifyPassword(loginDto.password, user.password)) {
+        const token = this.jwtService.signAsync({
+          email: user.email,
+          id: user.id,
+        });
+
+        delete user.password;
+        return { token, user };
+      } else {
+        throw new UnauthorizedException('Bad credentials - password not match');
+      }
+    }
+  }
+
+  async verifyPassword(password: string, hash: string) {
+    return await bcrypt.compare(password, hash);
+  }
+}
