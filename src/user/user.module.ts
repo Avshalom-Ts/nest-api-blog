7d5769fffe5c1f7a774e2a741d8e6fad1ec431f6
Azdevpp <Azdevpp@gmail.com>
2022-12-31 18:04:32 +0200

    Defined th packages in user.module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,18 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: 'secretKey',
+      signOptions: { algorithm: 'HS512', expiresIn: '1d' },
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
