3b5c198dec474c3cd66d894fb4cfba4a45c033f1
Azdevpp <Azdevpp@gmail.com>
2023-01-03 19:07:10 +0200

    Added function uploadPhoto + package npm i -D @types/multer and folder uploads

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -12,11 +12,15 @@ import {
   ClassSerializerInterceptor,
   Req,
   Query,
+  UploadedFile,
 } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { User } from 'src/user/entities/user.entity';
+import { Express } from 'express';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Controller('posts')
 export class PostController {
@@ -44,6 +48,31 @@ export class PostController {
   @Get('/slug/:slug')
   findBySlug(@Param('slug') slug: string) {
     return this.postService.findBySlug(slug);
+  }
+
+  @Post('upload-photo')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const name = file.originalname.split('.')[0];
+          const fileExtention = file.originalname.split('.')[1];
+          const newFileName =
+            name.split('').join('_') + '_' + Date.now() + '.' + fileExtention;
+          cb(null, newFileName);
+        },
+      }),
+      fileFilter: (req, file, cb) => {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+          return cb(null, false);
+        }
+        cb(null, true);
+      },
+    }),
+  )
+  uploadPhoto(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
   }
 
   @Patch(':slug')
